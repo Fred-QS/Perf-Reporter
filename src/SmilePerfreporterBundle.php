eece736c4a9fadfb7c33f69adda0e5989cc66b03
Fred-QS <lespaul96orange15coleen04>
2022-03-18 23:45:29 +0100

    Review

--- src/SmilePerfreporterBundle.php
+++ src/SmilePerfreporterBundle.php
@@ -3,6 +3,7 @@
 namespace Smile\PerfreporterBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Smile\PerfreporterBundle\DependencyInjection\PerfreporterExtension;
 
 class SmilePerfreporterBundle extends Bundle
