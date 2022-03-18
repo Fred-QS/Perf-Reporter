7d5b2c94016529a5b9da0f8f19d681919fd63aac
Fred-QS <lespaul96orange15coleen04>
2022-03-18 23:13:18 +0100

    Set structure

---
+++ src/EventSubscriber/ResponseSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Smile\PerfreporterBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ResponseSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => [
+                ['addSecurityHeaders', 0],
+            ],
+        ];
+    }
+
+    public function addSecurityHeaders(ResponseEvent $event)
+    {
+        $response = $event->getResponse();
+        $response->headers->set('X-Header-Set-By', 'Perfs Reporter');
+    }
+}
