ecdbd16ce88a15c0cc4c605a046f152803636cad
Fred-QS <in-touch-fg@quanticalsolutions.com>
2022-03-19 11:25:15 +0100

    adjust CSS

--- src/DomBuilders/TemplateBuilder.php
+++ src/DomBuilders/TemplateBuilder.php
@@ -10,6 +10,7 @@ class TemplateBuilder extends FileSystem
     protected static function setHTMLHeadTag(string $title) :string
     {
         date_default_timezone_set('Europe/Paris');
+        Carbon::setLocale('fr');
         ob_start(); ?>
         <!DOCTYPE html>
         <html lang="en_EN">
@@ -17,7 +18,7 @@ class TemplateBuilder extends FileSystem
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title><?= $title ?> at <?= date("Y-m-d H:i") ?></title>
+            <title><?= $title ?> au <?= Carbon::now()->isoFormat('LLL') ?></title>
             <style>
                 :root {
                     --primary-bg: steelblue;
@@ -27,18 +28,24 @@ class TemplateBuilder extends FileSystem
                     --hover-bg: lightblue;
                     --hover-color: #fff;
                 }
+                <?= file_get_contents(dirname(__DIR__, 2) . '/assets/reset.css') . "\n" ?>
                 <?= file_get_contents(dirname(__DIR__, 2) . '/assets/style.css') . "\n" ?>
             </style>
         </head>
         <body>
         <span id="export-pdf">Export PDF</span>
-        <h1><?= $title ?> at <?= date("Y-m-d H:i") ?></h1>
+        <section>
+        <div id="title">
+            <h1><?= $title ?></h1>
+            <i>au <?= Carbon::now()->isoFormat('LLL') ?></i>
+        </div>
         <?php return ob_get_clean();
     }
 
     protected static function setHTMLFooterTag() :string
     {
         ob_start(); ?>
+        </section>
         <script>
             <?= file_get_contents(dirname(__DIR__, 2) . '/assets/script.js') . "\n" ?>
         </script>
@@ -49,8 +56,51 @@ class TemplateBuilder extends FileSystem
 
     protected static function fillFileLines(string $file, array $header, array $steps) :string
     {
+        $header = self::parseToList($header);
         ob_start(); ?>
-        test
+        <?= $header ?>
         <?php return ob_get_clean();
+    }
+
+    private static function parseToList(array $arr) :string
+    {
+        $array = [];
+        foreach ($arr as $key => $value) {
+            if (!is_array($value) && !is_object($value) && !is_callable($value)) {
+                $array[$key] = $value;
+            } else {
+                $array[$key] = [];
+                foreach ($value as $label => $row) {
+                    if (!is_array($row) && !is_object($row) && !is_callable($row)) {
+                        $array[$key][ucfirst($label)] = (!is_null($row)) ? '<span class="set">' . $row . '</span>' : '<span class="not-set">Not set</span>';
+                    } else {
+                        $row = (array)$row;
+                        $array[$key][ucfirst($label)] = '<ul class="sub-lists">';
+                        foreach ($row as $k => $v) {
+                            if (!is_array($v) && !is_object($v) && !is_callable($v) && !is_null($v)) {
+                                $array[$key][ucfirst($label)] .= '<li><b>' . $k . '</b>: ' . $v . '</li>';
+                            }
+                        }
+                        $array[$key][ucfirst($label)] .= '</ul>';
+                    }
+                }
+            }
+        }
+
+        $html = '<div class="lists-container">';
+        foreach ($array as $key => $value) {
+            $html .= '<div class="list-bloc"><p class="list-title">' . ucfirst($key) . '</p><ul>';
+            if (!is_array($value) && !is_object($value) && !is_callable($value)) {
+                $html .= '<li>' . $value . '</li>';
+            } else {
+                foreach ($value as $k => $v) {
+                    $html .= '<li><b>' . ucfirst($k) . ':</b> ' . $v . '</li>';
+                }
+            }
+            $html .= '</ul></div>';
+        }
+        $html .= '</div>';
+
+        return $html;
     }
 }
